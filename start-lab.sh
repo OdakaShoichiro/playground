c9ac5758d1d48593e24faa51cfbae11e29e8181c
Shoichiro Odaka <instantlily@gmail.com>
2023-11-04 02:31:03 +0900

    update dependencies and port settings

--- start-lab.sh
+++ start-lab.sh
@@ -2,7 +2,7 @@
 
 jupyter lab /workspace/notebook \
     --no-browser \
-    --allow-root \
     --gpus all \
     --ip 0.0.0.0 \
-    --port 8888 \
+    --port 18888 \
+    --NotebookApp.token='' \
