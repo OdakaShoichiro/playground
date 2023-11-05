292c5e3580c94d939d1b777353a99be473468567
Shoichiro Odaka <instantlily@gmail.com>
2023-11-05 12:01:17 +0900

    add R package

--- setup-renv.R
+++ setup-renv.R
@@ -3,21 +3,22 @@
 renv::restore()
 
 install.packages(
-    c(
-        "languageserver",
-        "IRkernel",
-        "car",
-        "rpart",
-        "tidyverse",
-        "tseries"
-    ),
-    repos = "http://cran.rstudio.com/"
+  c(
+    "languageserver",
+    "IRkernel",
+    "car",
+    "rpart",
+    "tidyverse",
+    "tseries",
+    "urca"
+  ),
+  repos = "http://cran.rstudio.com/"
 )
 
 IRkernel::installspec(
-    name = "ir",
-    displayname = "R (renv)",
-    rprofile = "/workspace/.Rprofile"
+  name = "ir",
+  displayname = "R (renv)",
+  rprofile = "/workspace/.Rprofile"
 )
 
 renv::snapshot()
