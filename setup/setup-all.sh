839f8b33bab0422b897362c52592c8b245c9d8eb
Shoichiro Odaka <instantlily@gmail.com>
2026-01-02 01:42:27 +0900

    update base image

--- setup/setup-all.sh
+++ setup/setup-all.sh
@@ -6,8 +6,7 @@ mise settings add idiomatic_version_file_enable_tools "[]"
 mise install -y
 
 # Python
-rye config --set-bool behavior.use-uv=true
-rye sync --no-lock
+uv sync --no-lock
 
 # R
 R -e "install.packages('renv', repos = 'http://cran.rstudio.com/')"
@@ -16,8 +15,8 @@ Rscript /workspace/setup-renv.R
 # Julia
 julia --project='/workspace' -e 'using Pkg; Pkg.instantiate(); Pkg.build("IJulia")'
 
-# Node.js
-npm install
+# Bun
+bun install
 
 # Jupyter
 bash /workspace/setup/update-jupyter-template.sh
